dad6ffa31c00974c1512a2f7eeed33e8555fe88d
Benjamin Cyubahiro <benjamin.cyubahiro@amalitech.com>
2025-10-19 13:44:28 +0200

    fixed few comments

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,4 +1,3 @@
-// Export all configuration
 export { default as prisma, disconnectDatabase } from './database';
 export { env, isDevelopment, isProduction } from './env';
 export { swaggerSpec } from './swagger';
